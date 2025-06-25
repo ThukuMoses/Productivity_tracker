bb4cad910af093fe61e868c518daf5e5efadc18f
ThukuMoses <mosesrunanu9@gmail.com>
2025-06-25 22:33:57 +0300

    updated version with simple most productive day

--- personal_productivity_tracker.py
+++ personal_productivity_tracker.py
@@ -16,8 +16,8 @@ while True:
         print("Invalid time, Enter time in correct format!")
 
 # Collect daily input from the user
-Number_of_tasks_completed=int(input("How many tasks did you complete?: "))
-Focus_rate=int(input("What was you focus rate?(1-10): "))
+number_of_tasks_completed=int(input("How many tasks did you complete?: "))
+focus_rate=int(input("What was you focus rate?(1-10): "))
 notes= input("Any thoughts about your day?: ")
 
 filename="data.json"
@@ -25,8 +25,8 @@ filename="data.json"
 data= {
     "Date":today,
     "Wakeup_time":Wakeup_time,
-    "Number_of_tasks_completed":Number_of_tasks_completed,
-    "Focus":Focus_rate,
+    "Number_of_tasks_completed":number_of_tasks_completed,
+    "Focus":focus_rate,
     "Notes/thoughts":notes,
 }
 
@@ -49,10 +49,13 @@ existing_logs.append(data)
 
 # Save all logs back to the file
 with open(filename, "w") as f:
+    for i, log in enumerate(existing_logs):#addition of index to data dictionary
+        log['index']=i   
     json.dump(existing_logs, f, indent=3)
 
     for log in existing_logs:
-        print(f"\n\n\nDate: {log['Date']}\nWake_time: {log['Wakeup_time']}\nTasks completed: {log['Number_of_tasks_completed']}\nFocus: {log['Focus']}\nNotes/Thoughts: {log['Notes/thoughts']}")
+        print(f"\n\nDate: {log['Date']}\nWake_time: {log['Wakeup_time']}\nTasks completed: {log['Number_of_tasks_completed']}\nFocus: {log['Focus']}\nNotes/Thoughts: {log['Notes/thoughts']}\n\n")
+        # print("-"*40)#for visual clarity
 
 # --- Summary Analysis Section ---
     total_focus=0
@@ -74,3 +77,11 @@ with open(filename, "w") as f:
     def average():
         return total_focus/log_count if log_count >0 else 0#condition to avoid division by zero error
     print(f"Average focus:{average()}")
+
+    most_productive_day=max(totalTasks)
+    def productive_day():
+    
+        return most_productive_day
+    print(data)
+#print(productive_day)
+#print(f"Most productive day | {log['Date']} | (Number of tasks: {most_productive_day})")
